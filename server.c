18391642fc904c3b7700c65e2296859e51c972f7
Matan Shtepel <matan.shtepel@gmail.com>
2023-02-01 18:35:26 -0800

    requesting for all content types BUT PDF works fine... debugging

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define PORT 15635
 
@@ -22,7 +23,13 @@ char *content_types[6] = {
 void get_file_name_requested(char *http_request, char *file_name, char *content_type_name)
 {
     // we expect only GET requests, that should be formatted GET filename HTTP-version \r\n {other stuff}
-    assert(http_request[0] == 'G' && http_request[1] == 'E' && http_request[2] == 'T' && http_request[3] == ' ' && http_request[4] == '/');
+    if (!(http_request[0] == 'G' && http_request[1] == 'E' && http_request[2] == 'T' && http_request[3] == ' ' && http_request[4] == '/'))
+    {
+        // something we don't expect has occured
+        printf("CLIENT REQUEST \n ================================\n%s\n", http_request); // print client message for debugging purposes
+        printf("client request was not of the expected format");
+        exit(1);
+    }
     const int first_relevent_idx = 5;
     int i = 0;
     bool seen_dot = false;
@@ -46,86 +53,25 @@ void get_file_name_requested(char *http_request, char *file_name, char *content_
     file_name[i] = 0;
     content_type_name[j] = 0;
 
+    // serve default requests
     if (i == 0)
     {
         file_name = "index.html";
+        content_type_name = "html";
     }
 
-    printf("\nFILENAME REQUESTED: %s, i: %i\n", file_name, i);
+    printf("\nFILENAME REQUESTED: %s, CONTENT_TYPE_NAME:%s , I: %i\n", file_name, content_type_name, i);
 }
 
 int main(int argc, char *argv[])
 {
-
-    // this part up to line 57 not really applicable to project but just for testing different content types
-    // do command `./server filetype` with filetype being what filetype you want to show
-    // ex: ./server png will show a png file
-    // filetypes: html, txt, png, jpg, pdf bin
-    // for right now, bin is not displaying in browser (might be due to client browser prevention/blocking)
-
-    // for now, we are opening file before the server is setup
-    // later, we will be opening file after the server is setup and after we get file name from client HTTP request
-    // so this will probably be in the while in line 144
-    char file_name[1000] = "test"; //? this will be parsed from the request
-
-    int content_type;
-    // default
-    if (argc != 2)
-    {
-        assert("asserting no deffult for now");
-        strcat(file_name, "html");
-        content_type = 0;
-    }
-    else
-    {
-        if (strcmp(argv[1], "html") == 0)
-        {
-            strcat(file_name, ".html");
-            content_type = 0;
-        }
-        else if (strcmp(argv[1], "txt") == 0)
-        {
-            strcat(file_name, ".txt");
-            content_type = 1;
-        }
-        else if (strcmp(argv[1], "png") == 0)
-        {
-            strcat(file_name, ".png");
-            content_type = 2;
-        }
-        else if (strcmp(argv[1], "jpg") == 0)
-        {
-            strcat(file_name, ".jpg");
-            content_type = 3;
-        }
-        else if (strcmp(argv[1], "pdf") == 0)
-        {
-            strcat(file_name, ".pdf");
-            content_type = 4;
-        }
-        else if (strcmp(argv[1], "bin") == 0)
-        {
-            content_type = 5;
-        }
-        else
-        {
-            assert("this should not happen");
-        }
-    }
-
-    // opening file
-
-    FILE *file;
-
+    //* only declare server setting stuff out of loop
     // server socket descriptor
     int server_fd;
-
-    // defining socket's port and IP address
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
     // htons host-to-net short (16-bit) translation
-
     address.sin_family = AF_INET;         // iv4 (set to that, some internet prootcol)
     address.sin_addr.s_addr = INADDR_ANY; // set the ip address to any (which we can recieve from)
     address.sin_port = htons(PORT);       // translate the port to big-endien=16bit
@@ -156,76 +102,69 @@ int main(int argc, char *argv[])
     }
 
     // server starts listening and can have up to 3 max clients queued for backlog
-
+    //*done with setup -- listening
     if (listen(server_fd, 3) < 0)
     {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    // client socket
-
-    int new_socket;
-
-    // buffer for client HTTP request; will need to parse this later to get the file name
-    char buffer[1024] = {0};
-
-    // valread is just number of bytes returned from read() which we will call later
-    int valread;
-    // while loop cause server will keep listening for requests
+    // *while loop to listen to requests
     while (1)
     {
+        //*declare variables at the top of the loop because of overwriting fears
+        FILE *file;                        // for user's file
+        int client_socket;                 // client socket
+        char content_type_name[100] = {0}; // the type filename.{whatever} requested by client
+        char buffer[1024] = {0};           // buffer for client HTTP request;
+        char http_header[4096] = {0};      // header for http response
+        char dbuffer[4096];                // for sending file-chunks
+        char file_name[1000] = {0};        // the file_name requested by client
+        int valread = 0;                   // valread is just number of bytes returned from read() which we will call later
+        int file_size = 0;                 // client's file size
+
+        //* accept a client connection and read the message into the buffer
         // address and addrlen are result arguments that will have the client's info
-        // we redefined address and addrlen and we can probably use this later
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        if ((client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
         {
             perror("accept");
             exit(EXIT_FAILURE);
         }
-
+        valread = read(client_socket, buffer, 1024);
         // we read in the client's HTTP request and then we will parse this for the file name
-        valread = read(new_socket, buffer, 1024);
 
-        char *content_type_name = char[100];
+        printf("CLIENT REQUEST \n ================================\n%s\n", buffer); // print client message for debugging purposes
+
+        //*parse the client's request
         get_file_name_requested(buffer, file_name, content_type_name);
 
-        //* new stuff with per-file recording
-
+        //* open the file requested by clients, meassure it's length
+        printf("\nTRYING TO OPEN FILE: %s\n", file_name); // prints out http_header for debugging purposes
         file = fopen(file_name, "rb");
         if (file == NULL)
-        { //! maybe here we would want to give a 404
+        { //! maybe here we would want to give a 404, etc
 
             perror("fopen");
             return 1;
         }
 
-        // getting file size
-        int file_size;
-
+        // get the file size
         fseek(file, 0, SEEK_END);
         file_size = ftell(file);
         rewind(file);
 
-        // need http_header for proper HTTP response to client or there will be errors
-        char http_header[4096];
-        memset(http_header, 0, 4096);
+        //* build and send http header
 
         // set http_header with our file size and our desired content type
-        sprintf(http_header, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\nConnection: close\r\nContent-Type: %s\r\n\r\n", file_size, content_types[content_type]);
+        sprintf(http_header, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\nConnection: close\r\nContent-Type: %s\r\n\r\n", file_size, content_type_name);
 
-        // prints out http_header for debugging purposes
-        printf("%s", http_header);
-
-        // printing client request
-        printf("%s\n", buffer);
+        printf("%s", http_header); // prints out http_header for debugging purposes
 
         // we send client socket the HTTP header first; make sure to do strlen instead of size_of (spent some time trying to debug this)
-        send(new_socket, http_header, strlen(http_header), 0);
+        send(client_socket, http_header, strlen(http_header), 0);
 
         // we will send the file after in chunks of 4096 bytes
         // databuffer for reading and sending
-        char dbuffer[4096];
-        memset(dbuffer, 0, 4096);
 
         // in this chunk of code, we are checking if FILE* file descriptor is valid
         // then we will read in data from the file in chunks of 4096 bytes
@@ -236,19 +175,20 @@ int main(int argc, char *argv[])
         if (file > 0)
         {
             printf("call in loop of msg recieve");
+            memset(dbuffer, 0, 4096); // so we don't send something that is no longer file-- or whatever
             while ((bytes_read = fread(dbuffer, 1, 4096, file)) > 0)
             {
-                if (send(new_socket, dbuffer, bytes_read, 0) < 0)
+                if (send(client_socket, dbuffer, bytes_read, 0) < 0)
                 {
-                    perror("Error\n");
+                    perror("send:data-packets");
                     return 1;
                 }
             }
         }
 
-        close(new_socket);
+        close(client_socket);
+        fclose(file);
     }
-    fclose(file);
 
     return 0;
 }
